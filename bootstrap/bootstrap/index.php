355718346a99b9e35d6048ca3b4449a7115c0d38
Sladan Nencic <Sladan@hotmail.de>
2013-11-21 19:32:46 +0100

    Version 1.8

---
+++ bootstrap/bootstrap/index.php
@@ -0,0 +1,29 @@
+<?php
+	echo "Hausafugabe";
+?>
+
+    <!DOCTYPE html>
+    <html>
+    	<head>
+    		<title>Bootstrap 101 Template</title>
+    			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    			<!-- Bootstrap -->
+    			<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    	</head>
+   		
+   		<body>
+    		<h1>Hello, world!</h1>
+    		<h3>Descriptio Helvetia</h3>
+    		<p>Undique loci natura Helvetii continentur: Una ex parte flumine Rheno latissimo atque altissimo, 
+    			qui agrum Helvetium a Germanis dividit; altera ex parte monte Iura altissimo, 
+    			qui est inter Sequanos et Helvetios; tertia lacu Lemanno et flumine Rhodano, 
+    			qui provinciam nostram ab Helvetiis dividit. His rebus fiebat ut et minus late vagarentur et minus 
+    			facile finitimis bellum inferre possent. Qua ex parte homines bellandi cupidi magno dolore adficiebantur.
+				Pro multitudine autem hominum et pro gloria belli atque fortitudinis angustos se fines habere arbitrabantur, 
+				qui in longitudinem milia passuum CCXL, in latitudinem CLXXX patebant.</p>
+    		<script src="http://code.jquery.com/jquery.js"></script>
+    		<script src="js/bootstrap.min.js"></script>
+    	</body>
+   </html>
+
+
